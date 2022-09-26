60a9a35fc040cc1eeeba41ab6bd27a4faa9efc9a
Daniel Mathisen Breivik <danielmbreivik@hotmail.com>
2022-09-26 14:22:42 +0000

    new file:   .devcontainer/Dockerfile 	new file:   .devcontainer/base.Dockerfile 	new file:   .devcontainer/devcontainer.json 	modified:   haikus.json 	modified:   index.js 	modified:   package-lock.json

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ let ejs = require('ejs');
 const haikus = require('./haikus.json');
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
+  res.render('index', { haikus: haikus });
 });
 
 app.listen(port);
